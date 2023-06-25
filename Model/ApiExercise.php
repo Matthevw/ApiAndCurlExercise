d250bcbdad1d2ec8890147ad079f35a403aa17c3
Mateusz Sabała <mateusz.sabala@onet.pl>
2023-06-25 18:45:11 +0200

    Initial commit

---
+++ Model/ApiExercise.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace M2M\ApiAndCurlExercise\Model;
+
+use \Magento\Framework\HTTP\Client\Curl;
+
+class ApiExercise
+{
+    /**
+     * @var Curl
+     */
+    protected $curl;
+
+    public function __construct (
+        Curl $curl,
+    ) {
+        $this->curl = $curl;
+    }
+
+    public function getApiData()
+    {
+        $url = "https://api.punkapi.com/v2/beers/random";
+
+        $this->curl->get($url);
+
+        $result = $this->curl->getBody();
+
+        var_dump($result);
+
+        $json_string = json_decode($result, true);
+        // var_dump($json_string);
+
+        print(print_r($json_string,true)); die;
+        // $json_string = json_encode($result, JSON_PRETTY_PRINT);
+
+    }
+
+}
